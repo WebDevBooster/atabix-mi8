c887834696807eca3b635d52a5c08034cad3d1aa
Paul Hage <paul.hage@atabix.com>
2014-12-21 23:08:13 +0100

    Upgrade Timecalendar
    Signed-off-by: Paul Hage <paul.hage@atabix.com>

--- js/init.js
+++ js/init.js
@@ -1,6 +1,23 @@
+$(document).foundation();
+
 $(function() {
+    $("#st-container").niceScroll({
+        cursorborder:"0",
+        touchbehavior: false,
+        zindex: "999999999999",
+        railpadding: {right: 2},
+        cursorborderradius: "5px",
+        bouncescroll: true,
+        scrollspeed: "60"
+    });
+    
     $('.datepicker').datepicker({
-        fistday: 1
+        changeMonth: true,
+        changeYear: true,
+        firstDay: 1,
+        buttonImage: "static/images/loading.gif",
+        dateFormat: 'dd-mm-yy',
+        showButtonPanel: true
     });
     
     $("#globalSearch").globalSearch();
@@ -8,4 +25,9 @@ $(function() {
     Mousetrap.bind('command+shift+s', function(e) {
         $("#globalSearch").globalSearch('open');
     });
+    
+    $(".URLify").dblclick(function(){
+        window.location=$(this).find("a").attr("href");
+        return false;
+    });        
 });
