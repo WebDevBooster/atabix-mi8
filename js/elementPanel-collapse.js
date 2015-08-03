b1447dcecf91becffc17af55b40eda7a75d4e4a9
Jasper Zuyderwijk <jasper.zuyderwijk@atabix.nl>
2015-08-03 11:20:00 +0200

    grunt build

--- js/elementPanel-collapse.js
+++ js/elementPanel-collapse.js
@@ -2,5 +2,7 @@ $(function() {
     $( ".elementPanel.collapsable .title" ).click(function(){
         $( this ).toggleClass('open');
         $( this ).next('.content').slideToggle(700, "easeOutBounce");
+        $(document).foundation('equalizer','reflow');
+        equalizeNow();
     });
 });
