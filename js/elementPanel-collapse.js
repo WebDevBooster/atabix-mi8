0a3a5761200a0ff4839ad76c964ec1cbad84b0f9
Jasper Zuyderwijk <jasper.zuyderwijk@atabix.nl>
2015-08-03 11:44:14 +0200

    notification collapse js

--- js/elementPanel-collapse.js
+++ js/elementPanel-collapse.js
@@ -1,6 +1,6 @@
 $(function() {
     $( ".elementPanel.collapsable .title" ).click(function(){
-        $( this ).toggleClass("open", "closed");
+        $( this ).toggleClass("closed");
         $( this ).next('.content').slideToggle(700, "easeOutBounce");
 
         $(document).foundation('equalizer','reflow');
--- js/notification-collapse.js
+++ js/notification-collapse.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     $('.notification .main').click(function() {
-        $('.notification').switchClass( "closed", "open", 700, "easeOutBounce");
+        $('.notification').switchClass("closed", "open", 700, "easeInOutExpo");
         setTimeout(function(){
             $(document).foundation('equalizer','reflow');
             equalizeNow();
         }, 200);
     });
     $('.notification .closeNotification').click(function() {
-        $('.notification').switchClass("open", "closed", 700, "easeOutBounce");
+        $('.notification').switchClass("open", "closed", 700, "easeInOutExpo");
         setTimeout(function(){
             $(document).foundation('equalizer','reflow');
             equalizeNow();
