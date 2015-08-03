27052c4d89bdc3a46f851b4d4ed9aa5ad2bdf764
Jasper Zuyderwijk <jasper.zuyderwijk@atabix.nl>
2015-08-03 11:54:56 +0200

    200ms easeOutExpo

--- js/notification-collapse.js
+++ js/notification-collapse.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     $('.notification .main').click(function() {
-        $('.notification').switchClass("closed", "open", 700, "easeInOutExpo");
+        $('.notification').switchClass("closed", "open", 200, "easeInOutExpo");
         setTimeout(function(){
             $(document).foundation('equalizer','reflow');
             equalizeNow();
         }, 200);
     });
     $('.notification .closeNotification').click(function() {
-        $('.notification').switchClass("open", "closed", 700, "easeInOutExpo");
+        $('.notification').switchClass("open", "closed", 200, "easeInOutExpo");
         setTimeout(function(){
             $(document).foundation('equalizer','reflow');
             equalizeNow();
