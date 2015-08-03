57e73ea90ce8c93fae24b6ccf76cb89d4190cb00
Jasper Zuyderwijk <jasper.zuyderwijk@atabix.nl>
2015-08-03 13:12:59 +0200

    grunt js build

--- js/elementPanel-collapse.js
+++ js/elementPanel-collapse.js
@@ -1,6 +1,6 @@
 $(function() {
     $( ".elementPanel.collapsable .title" ).click(function(){
-        $( this ).toggleClass("closed");
+        $( this ).switchClass("open", "closed");
         $( this ).next('.content').slideToggle(700, "easeOutBounce");
 
         $(document).foundation('equalizer','reflow');
