99821247f9ba46d54558282a6f45f5bc6feb942c
Jasper Zuyderwijk <jasper.zuyderwijk@atabix.nl>
2015-08-03 11:34:56 +0200

    elementPanel switchClass toggle

--- js/elementPanel-collapse.js
+++ js/elementPanel-collapse.js
@@ -1,7 +1,8 @@
 $(function() {
     $( ".elementPanel.collapsable .title" ).click(function(){
-        $( this ).toggleClass('open');
+        $( this ).switchClass("open", "closed");
         $( this ).next('.content').slideToggle(700, "easeOutBounce");
+
         $(document).foundation('equalizer','reflow');
         equalizeNow();
     });
