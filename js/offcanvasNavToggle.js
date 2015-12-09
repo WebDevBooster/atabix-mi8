462e204316c487b8963fdf892bf914b774acdfcb
Lusenet <lusenet@atabix.nl>
2015-12-09 15:06:58 +0100

    oude functie weggehaald

--- js/offcanvasNavToggle.js
+++ js/offcanvasNavToggle.js
@@ -1,9 +1,3 @@
-$('.offcanvas ul.menu .has-subs').click(function() {
-    $(this).toggleClass('active');
-    $(this).find('.submenu').slideToggle();
-    $(this).parent().siblings().next().slideToggle();
-});
-
 $('.offcanvas').on('click', 'span', function(){
     $(this).toggleClass('fa-plus').toggleClass('fa-minus');
     $(this).find('.submenu').slideToggle(100); 
