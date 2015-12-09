a4a17bdbde95e8c173ac264f5e4d1cea79d06685
Lusenet <lusenet@atabix.nl>
2015-12-09 14:59:34 +0100

    generate spans

--- js/offcanvasNavToggle.js
+++ js/offcanvasNavToggle.js
@@ -4,7 +4,12 @@ $('.offcanvas ul.menu .has-subs').click(function() {
     $(this).parent().siblings().next().slideToggle();
 });
 
-$('.offcanvas span').on('click', function(){
+$('.offcanvas').on('click', 'span', function(){
     $(this).toggleClass('fa-plus').toggleClass('fa-minus');
     $(this).find('.submenu').slideToggle(100); 
+});
+
+$('.offcanvas ul li.has-subs').each(function(){
+    var span = $(document.createElement('span')).addClass('fa fa-plus');
+    $(this).prepend(span); 
 });
